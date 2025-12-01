5f7967edcee04f3699a86668ab2ab01ff50d2128
AlexM <alejandromadrigal1409@gmail.com>
2025-11-30 20:53:49 -0600

    gurobi con tiempos de llegada

--- proyect_main.py
+++ proyect_main.py
@@ -1,7 +1,8 @@
 # NOMECLATURA:
 # n = número de tareas
 # k = número de procesadores
-# t[] = duración de tareas
+# d[] = duración de tareas
+# s[] = tiempo de llegada de las tareas
 
 # CONSIDERACIONES:
 # probar n = 50, 100, 200, 400
@@ -13,7 +14,7 @@ import numpy as np
 
 def generador_instancias(n):
   np.random.seed(47)
-  mu_nom = 0
+  mu_nom = 3
   sigma_nom = 1
   mu_lognom = 0
   sigma_lognom = 1
@@ -21,24 +22,21 @@ def generador_instancias(n):
   distribucion_normal(mu_nom, sigma_nom, n)
   distribucion_lognormal(mu_lognom, sigma_lognom, n)
 
+# Distribución para duración de tareas
 def distribucion_normal(mu, sigma, n):
-  print("\nDistribución Normal\n")
-  t = []
+  print("\nDuración de tareas\n")
+  d = []
   for i in n:
-   t.append(np.random.normal(mu, sigma, i))
-  print(t)
+   d.append(np.random.normal(loc = mu, scale = sigma, size = i))
+  print(d)
 
+# Distribución para llegada de tareas
 def distribucion_lognormal(mu, sigma, n):
-  print("\nDistribución LogNormal\n")
-  t = []
+  print("\nTiempo de llegada de tareas\n")
+  s = []
   for i in n:
-   t.append(np.random.lognormal(mu, sigma, i))
-  print(t)
-  #if flag == 1:
-  #t = np.random.exponential(scale = 1/lam, size = n)
-  #print(t)
-  #u = np.random.random(size = n)
-  #t = (-1/lam)*np.log(1 - u)
-  #print(t)
+   s.append(np.random.lognormal(mean = mu, sigma = sigma, size = i))
+  print(s)
 
 generador_instancias([50, 100, 200, 400])
+
