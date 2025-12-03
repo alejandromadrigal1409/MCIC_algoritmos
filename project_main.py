e537352d9201985e52e402b878c8e3398e8a2b6b
Alejandro <alejandromadrigal1409@gmail.com>
2025-12-03 14:15:19 -0600

    función para crear grafica de tiempo de ejecuciçión

--- project_main.py
+++ project_main.py
@@ -15,7 +15,7 @@ from gurobipy import GRB
 import heapq
 from scipy.stats import t
 import matplotlib.pyplot as plt
-
+import time
 
 def generador_instancias(dist, n):
   #np.random.seed(69)
@@ -38,7 +38,6 @@ def distribucion_normal(mu, sigma, n):
    l.append(np.random.normal(loc = mu, scale = sigma, size = i))
 
   return l 
-  #print(d)
 
 # ----------------- DISTRIBUCIÓN LOGNORMAL -----------------
 def distribucion_lognormal(mu, sigma, n):
@@ -47,13 +46,13 @@ def distribucion_lognormal(mu, sigma, n):
    l.append(np.random.lognormal(mean = mu, sigma = sigma, size = i))
 
   return l
-  #print(a)
 
 # ----------------- SOLUCIÓN CON GUROBI -----------------
 def solucion_gurobi(m, N, L):
-  #print("SOLUCIÓN GUROBI")
   makespans = []
+  tiempo = []
   for aux in range(len(N)):
+    inicio = time.time()
     n = N[aux]
     l = L[aux]
 
@@ -77,22 +76,20 @@ def solucion_gurobi(m, N, L):
     model.setParam('MIPGap', 0.001)
     model.optimize()
 
-    # Mostrar solución
-    #print("="*150)
-    #print(f"\nSolución para {n} tareas")
-    #print(f"\nMakespan óptimo = {Makespan.X}")
-    # Gudar soluciones
+    fin = time.time()
+
     makespans.append([Makespan.X])
+    tiempo.append([fin - inicio])
   
-  return np.array(makespans)
+  return np.array(makespans), np.array(tiempo)
 
 # ----------------- SOLUCIÓN CON GREEDY 1.5 -----------------
 def solucion_greedy_1_5(m, N, L):
-  #print("\n\nSOLUCIÓN GREEDY 1.5")
   makespans = []
+  tiempo = []
   for aux in range(len(N)):
-    n = N[aux]
     l = L[aux]
+    inicio = time.time()
 
     l = sorted(l, reverse=True) 
 
@@ -105,23 +102,21 @@ def solucion_greedy_1_5(m, N, L):
         heapq.heappush(heap, load) # creo min-heap con los dato nuevos
 
     Makespan = max(heap)
-    # Mostrar solución
-    #print("="*150)
-    #print(f"\nSolución para {n} tareas")
-    #print(f"\nMakespan óptimo = {Makespan}")
 
-    # Gudar soluciones
+    fin = time.time()
+ 
     makespans.append([Makespan])
+    tiempo.append([fin - inicio])
   
-  return np.array(makespans)
+  return np.array(makespans), np.array(tiempo)
 
 # ----------------- SOLUCIÓN CON GREEDY 2 -----------------
 def solucion_greedy_2(m, N, L):
-  #print("\n\nSOLUCIÓN GREEDY 2")
   makespans = []
+  tiempo = []
   for aux in range(len(N)):
-    n = N[aux]
     l = L[aux]
+    inicio = time.time()
 
     heap = [0] * m  # crea una lista de ceros de tamaño m (carga de trabajo en cada procesador)
 
@@ -131,14 +126,17 @@ def solucion_greedy_2(m, N, L):
         heapq.heappush(heap, load) # creo min-heap con los dato nuevos
 
     Makespan = max(heap)
+
+    fin = time.time()
     # Mostrar solución
     #print("="*150)
     ##print(f"\nMakespan óptimo = {Makespan}")
   
     # Gudar soluciones
     makespans.append([Makespan])
+    tiempo.append([fin - inicio])
   
-  return np.array(makespans)
+  return np.array(makespans), np.array(tiempo)
 
 # ----------------- PROMEDIO E INTERVALO DE CONFIANZA -----------------
 def mediaIntervaloConfianza(makespans):
@@ -166,7 +164,6 @@ def mediaIntervaloConfianza(makespans):
 
 # ----------------- GRAFICA MAKESPAN NORMALIZADO -----------------
 def grafMakespanNorm(n, varStat, varStat_gurobi,leyenda):
-  #
   makespan = []
   lb = []
   up = []
@@ -178,14 +175,47 @@ def grafMakespanNorm(n, varStat, varStat_gurobi,leyenda):
     up.append(aux[2])
 
   plt.errorbar(n, makespan,
-             yerr=[np.array(makespan) - np.array(lb),
-                   np.array(up) - np.array(makespan)],  # [abajo, arriba]
-             label=leyenda,
-             fmt='-o',               # línea + círculos
-             linewidth=2, markersize=8,
-             capsize=4,              # "gorritos" en los extremos
-             capthick=1.5,
-             alpha=0.9)
+              yerr=[np.array(makespan) - np.array(lb),
+                    np.array(up) - np.array(makespan)],  # [abajo, arriba]
+              label=leyenda,
+              fmt='-o',               # línea + círculos
+              linewidth=2, markersize=8,
+              capsize=4,              # "gorritos" en los extremos
+              capthick=1.5,
+              alpha=0.9)
+  
+# ----------------- GRAFICA TIEMPOS DE EJECUCIÓN -----------------
+def graficaTiempos(n, varStat,leyenda):
+  tiempo = []
+  lb = []
+  up = []
+  for i in range(len(varStat)):
+    aux = varStat[i]
+    tiempo.append(aux[0])
+    lb.append(aux[1])
+    up.append(aux[2])
+
+  n      = [0] + n 
+  tiempo = [0] + tiempo
+  lb     = [0] + lb
+  up     = [0] + up
+
+  plt.plot(n, tiempo,
+           label=leyenda,
+           linewidth=2,
+           marker="o",       # tipo de punto
+           markersize=8)     # tamaño del punto
+  '''
+  plt.errorbar(n, makespan,
+              yerr=[np.array(makespan) - np.array(lb),
+                    np.array(up) - np.array(makespan)],  # [abajo, arriba]
+              label=leyenda,
+              fmt='-o',               # línea + círculos
+              linewidth=2, markersize=8,
+              capsize=4,              # "gorritos" en los extremos
+              capthick=1.5,
+              alpha=0.9) 
+  '''
 
 # ----------------- CÓDIGO PRINCIPAL -----------------
 N = [50, 100, 200, 400] # vector con número de tareas
@@ -196,35 +226,63 @@ print("Distribución normal    (0)")
 print("Distribución lognormal (1)")
 dist = input("Selecione una distribución para generar las duraciónde las tareas: ")
 
-makespans_gurobi = [[]] * len(N)
+makespans_gurobi     = [[]] * len(N)
 makespans_greedy_1_5 = [[]] * len(N)
-makespans_greedy_2 = [[]] * len(N)
+makespans_greedy_2   = [[]] * len(N)
+
+tiempos_gurobi     = [[]] * len(N)
+tiempos_greedy_1_5 = [[]] * len(N)
+tiempos_greedy_2   = [[]] * len(N)
 
 # Encontrar solución para 10 instancias
 for _ in range(10):
   L = generador_instancias(dist, N)
-  makespans_gurobi = np.hstack((makespans_gurobi,solucion_gurobi(m, N, L)) )
-  makespans_greedy_1_5 = np.hstack((makespans_greedy_1_5,solucion_greedy_1_5(m, N, L)))
-  makespans_greedy_2 = np.hstack((makespans_greedy_2,solucion_greedy_2(m, N, L)))
+  tmp_makespans_gurobi, tmp_tiempos_gurobi = solucion_gurobi(m, N, L)
+  makespans_gurobi = np.hstack((makespans_gurobi, tmp_makespans_gurobi))
+  tiempos_gurobi   = np.hstack((tiempos_gurobi, tmp_tiempos_gurobi))
 
-# LLamada a función para calcular promedio e IC
-varStat_gurobi = mediaIntervaloConfianza(makespans_gurobi)
-varStat_greedy_1_5 = mediaIntervaloConfianza(makespans_greedy_1_5)
-varStat_greedy_2 = mediaIntervaloConfianza(makespans_greedy_2)
+  tmp_makespans_greedy_1_5, tmp_tiempos_greedy_1_5 = solucion_greedy_1_5(m, N, L)
+  makespans_greedy_1_5 = np.hstack((makespans_greedy_1_5, tmp_makespans_greedy_1_5))
+  tiempos_greedy_1_5   = np.hstack((tiempos_greedy_1_5, tmp_tiempos_greedy_1_5))
 
-# LLamada a función para gráficar
+  tmp_makespans_greedy_2, tmp_tiempos_greedy_2 = solucion_greedy_2(m, N, L)
+  makespans_greedy_2  = np.hstack((makespans_greedy_2, tmp_makespans_greedy_2))
+  tiempos_greedy_2    = np.hstack((tiempos_greedy_2, tmp_tiempos_greedy_2))
+
+# LLamada a función para calcular promedio e IC de los makespans
+varStat_gurobi_makespans      = mediaIntervaloConfianza(makespans_gurobi)
+varStat_greedy_1_5_makespans  = mediaIntervaloConfianza(makespans_greedy_1_5)
+varStat_greedy_2_makespans    = mediaIntervaloConfianza(makespans_greedy_2)
+
+# LLamada a función para calcular promedio e IC de los tiempos de ejecución
+varStat_gurobi_tiempos     = mediaIntervaloConfianza(tiempos_gurobi)
+varStat_greedy_1_5_tiempos = mediaIntervaloConfianza(tiempos_greedy_1_5)
+varStat_greedy_2_tiempos   = mediaIntervaloConfianza(tiempos_greedy_2)
+
+# LLamada a función para gráficar Makespan normalizado
 plt.figure(figsize=(8, 5), num="Comparación: Gurobi vs Greedy") 
-grafMakespanNorm(N, varStat_gurobi,     varStat_gurobi, "Gurobi")
-grafMakespanNorm(N, varStat_greedy_1_5, varStat_gurobi, "Greedy 1.5 aprox")
-grafMakespanNorm(N, varStat_greedy_2, varStat_gurobi,   "Greedy 2 aprox")
-
-
+grafMakespanNorm(N, varStat_gurobi_makespans,     varStat_gurobi_makespans, "Gurobi")
+grafMakespanNorm(N, varStat_greedy_1_5_makespans, varStat_gurobi_makespans, "Greedy 1.5 aprox")
+grafMakespanNorm(N, varStat_greedy_2_makespans,   varStat_gurobi_makespans, "Greedy 2 aprox")
 plt.title("n vs Makespan") # Título
 plt.xlabel("n (número de tareas)") # Nombre del eje X
 plt.ylabel("Makespan normalizado") # Nombre del eje y
 plt.ylim(0, None)
 plt.legend()  # Muestra la leyenda
 plt.grid(True)
+
+# LLamada a función para gráficar tiempos de ejecución
+plt.figure(figsize=(8, 5), num="Comparación: Tiempo Gurobi vs Tiempo Greedy") 
+#graficaTiempos(N, varStat_gurobi_tiempos,     "Gurobi")
+graficaTiempos(N, varStat_greedy_1_5_tiempos, "Greedy 1.5 aprox")
+#graficaTiempos(N, varStat_greedy_2_tiempos,   "Greedy 2 aprox")
+plt.title("n vs Tiempo") # Título
+plt.xlabel("n (número de tareas)") # Nombre del eje X
+plt.ylabel("tiempo (segundos)") # Nombre del eje y
+plt.ylim(0, None)
+plt.legend()  # Muestra la leyenda
+plt.grid(True)
+
 plt.show()
 
 
